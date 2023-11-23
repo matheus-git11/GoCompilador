07545390045a30a8ff6f3c82817ef9f377931b2a
Bone Lira <bone-lira@hotmail.com>
2023-11-23 20:14:59 -0300

    refac: avoiding duplicate instanciation of AnalisadorLexico problem

--- src/AnalisadorSintatico.java
+++ src/AnalisadorSintatico.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.util.*;
 
-
 public class AnalisadorSintatico {
     private final AnalisadorLexico lexico;
     private String currentToken;
@@ -17,9 +16,8 @@ public class AnalisadorSintatico {
 
             String currentToken = tokens.get(i);
 
-            if (currentToken.equals("var")){
+            if (currentToken.equals("var")) {
                 i++;
-
 
                 // Verifica se o próximo token é um identificador válido
                 if (i < tokens.size() && tokens.get(i).matches("[a-zA-Z_]+")) {
@@ -29,7 +27,10 @@ public class AnalisadorSintatico {
                 }
 
                 // Verifica se o próximo token é um tipo válido (int, string, etc.)
-                if (i < tokens.size() && (tokens.get(i).equals("int") || tokens.get(i).equals("string") /* Outros tipos possíveis */)) {
+                if (i < tokens.size() && (tokens.get(i).equals("int") || tokens.get(i).equals("string") /*
+                                                                                                         * Outros tipos
+                                                                                                         * possíveis
+                                                                                                         */)) {
                     i++;
                 } else {
                     return false;
@@ -42,8 +43,6 @@ public class AnalisadorSintatico {
 
         return true; // Se percorreu todos os tokens sem problemas, retorna true
     }
-
-
 
     public boolean verificaFuncMain() {
         boolean temMain = false;
@@ -66,15 +65,15 @@ public class AnalisadorSintatico {
     }
 
     public boolean verificaEstruturaFunc() throws IOException {
-        AnalisadorLexico lex = new AnalisadorLexico();
+
         boolean estruturaCorreta = false;
 
-        while (lex.hasNextToken()) {
-            String token = lex.nextToken();
+        while (lexico.hasNextToken()) {
+            String token = lexico.nextToken();
 
             if (token.equals("func")) {
                 // Verifica se há um identificador após a palavra-chave 'func'
-                if (lex.hasNextToken() && lex.tokenType(lex.nextToken()).equals("identifier")) {
+                if (lexico.hasNextToken() && lexico.tokenType(lexico.nextToken()).equals("identifier")) {
                     estruturaCorreta = true; // Estrutura func + nome está correta
                 } else {
                     return false; // Nome de função ausente ou inválido após 'func'
@@ -84,7 +83,6 @@ public class AnalisadorSintatico {
 
         return estruturaCorreta; // Retorna true se a estrutura func + nome for encontrada
     }
-
 
     public boolean verificaChavesParantesesFechados() {
         Stack<String> pilha = new Stack<>();
@@ -113,84 +111,83 @@ public class AnalisadorSintatico {
         return pilha.isEmpty(); // Verifica se todos os símbolos foram fechados corretamente
     }
 
-
-        private void advanceToken() {
-            if (lexico.hasNextToken()) {
-                currentToken = lexico.nextToken();
-            } else {
-                currentToken = null;
-            }
+    private void advanceToken() {
+        if (lexico.hasNextToken()) {
+            currentToken = lexico.nextToken();
+        } else {
+            currentToken = null;
         }
+    }
 
-        private boolean matchToken(String expectedToken) {
-            if (currentToken != null && currentToken.equals(expectedToken)) {
-                advanceToken();
-                return true;
-            }
-            return false;
+    private boolean matchToken(String expectedToken) {
+        if (currentToken != null && currentToken.equals(expectedToken)) {
+            advanceToken();
+            return true;
         }
+        return false;
+    }
 
-        // Método para a regra 'expression'
-        private void expression() {
+    // Método para a regra 'expression'
+    private void expression() {
+        term();
+        while (currentToken != null && (currentToken.equals("+") || currentToken.equals("-"))) {
+            advanceToken();
             term();
-            while (currentToken != null && (currentToken.equals("+") || currentToken.equals("-"))) {
-                advanceToken();
-                term();
-            }
         }
+    }
 
-        // Método para a regra 'term'
-        private void term() {
+    // Método para a regra 'term'
+    private void term() {
+        factor();
+        while (currentToken != null && (currentToken.equals("*") || currentToken.equals("/"))) {
+            advanceToken();
             factor();
-            while (currentToken != null && (currentToken.equals("*") || currentToken.equals("/"))) {
-                advanceToken();
-                factor();
-            }
         }
+    }
 
-        // Método para a regra 'factor'
-        private void factor() {
-            if (currentToken != null && currentToken.matches("\\d+")) {
-                advanceToken();
-            } else if (matchToken("(")) {
-                expression();
-                if (!matchToken(")")) {
-                    // Erro: Esperado ')' após expressão
-                    // Tratamento de erro aqui
-                }
-            } else {
-                // Erro: Token inesperado
+    // Método para a regra 'factor'
+    private void factor() {
+        if (currentToken != null && currentToken.matches("\\d+")) {
+            advanceToken();
+        } else if (matchToken("(")) {
+            expression();
+            if (!matchToken(")")) {
+                // Erro: Esperado ')' após expressão
                 // Tratamento de erro aqui
             }
+        } else {
+            // Erro: Token inesperado
+            // Tratamento de erro aqui
         }
+    }
 
-        // Método para a regra 'ifStatement'
-        private void ifStatement() {
-            if (matchToken("if")) {
-                if (!matchToken("(")) {
-                    // Erro: Esperado '(' após 'if'
-                    // Tratamento de erro aqui
-                }
-                expression();
-                if (!matchToken(")")) {
-                    // Erro: Esperado ')' após expressão do 'if'
-                    // Tratamento de erro aqui
-                }
-                ifStatement();
-                if (matchToken("else")) {
-                    ifStatement();
-                }
+    // Método para a regra 'ifStatement'
+    private void ifStatement() {
+        if (matchToken("if")) {
+            if (!matchToken("(")) {
+                // Erro: Esperado '(' após 'if'
+                // Tratamento de erro aqui
             }
-        }
-
-        // Método para iniciar a análise sintática
-        public void parse() {
+            expression();
+            if (!matchToken(")")) {
+                // Erro: Esperado ')' após expressão do 'if'
+                // Tratamento de erro aqui
+            }
             ifStatement();
-            if (currentToken != null) {
-                // Erro: Tokens extras no final
-                // Tratamento de erro aqui
-            } else {
-                System.out.println("Análise sintática concluída com sucesso!");
+            if (matchToken("else")) {
+                ifStatement();
             }
         }
+    }
+
+    // Método para iniciar a análise sintática
+    public void parse() {
+        ifStatement();
+        if (currentToken != null) {
+            // Erro: Tokens extras no final
+            // Tratamento de erro aqui
+        } else {
+            System.out.println("Análise sintática concluída com sucesso!");
+        }
+    }
 }
