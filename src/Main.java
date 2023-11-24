d218b7e3c8d9180986282f221f4285f3b27fd06b
Bone Lira <bone-lira@hotmail.com>
2023-11-24 02:23:58 -0300

    typo: little adjustment

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,17 @@ public class Main {
             String tipo = analisador.tokenType(token);
             System.out.println("<" + tipo + "> " + token + " </" + tipo + ">");
 
-
         }
-            AnalisadorSintatico parser = new AnalisadorSintatico();
-            boolean main = parser.verificaFuncMain();
-            boolean symbol = parser.verificaChavesParantesesFechados();
-            boolean funcStructure = parser.verificaEstruturaFunc();
-            boolean variavel = parser.verificaDeclaracaoVariaveis(tokens);
+        AnalisadorSintatico parser = new AnalisadorSintatico();
+        boolean main = parser.verificaFuncMain();
+        boolean symbol = parser.verificaChavesParantesesFechados();
+        boolean funcStructure = parser.verificaEstruturaFunc();
+        boolean variavel = parser.verificaDeclaracaoVariaveis(tokens);
 
+        System.out.println("Funcao Main = " + main);
+        System.out.println("Funcao verificaChavesParantesesFechados = " + symbol);
+        System.out.println("Funcao funcStructure= " + funcStructure);
+        System.out.println("Funcao variavel= " + variavel);
 
-            System.out.println("Funcao Main = " + main);
-            System.out.println("Funcao verificaChavesParantesesFechados= " + symbol);
-            System.out.println("Funcao funcStructure= " + funcStructure);
-            System.out.println("Funcao variavel" + variavel);
-
-
-        }
     }
-
+}
